1939a23aae1448daa7f03a4d3d9919762a37c84b
Eric <tuyishimireericc@gmail.com>
2022-12-13 22:49:06 +0200

    Created rails react

---
+++ app/javascript/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+import Greeting from './pages/Greeting';
+
+const App = () => {
+  return (
+    <div>
+      <Greeting />
+    </div>
+  );
+};
+export default App;
